0c8178a8f98fe1dc4869bc8c2a422b0dd33219ee
Jagadeesh Madinni <jmadinn@clemson.edu>
2024-01-18 16:45:51 -0500

    Update Spring Damper Mass model

--- spring-damper-mass/spring_damper_system.m
+++ spring-damper-mass/spring_damper_system.m
@@ -5,3 +5,19 @@ k = 1; % Spring constant = 1.0 N/m
 b = 0.2; % Damping constant = 0.2 Ns/m
 F = 1; % Input Force = 1.0 N
 
+
+tspan = [0 5];
+y_init = [0.1;0];
+
+[t,y] = ode45(@springDamperResponse,tspan, y_init);
+
+plot(t,y(:,1));
+grid on;
+xlabel("time");
+ylabel("Displacement")
+title("Displacement vs time");
+
+
+function Y = springDamperResponse(y)
+Y = [y(2);F/m-(b/m)*y(2)-(k/m)*y(1)];
+end 
