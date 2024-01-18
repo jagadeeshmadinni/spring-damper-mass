b50ebb279e9c871b924d1d8f482a8c8c89ea6e48
ultraserenejoe <jmadinn@node1719.palmetto.clemson.edu>
2024-01-18 15:15:55 -0500

    Add Matlab script file

---
+++ spring-damper-mass/spring_damper_system.m
@@ -0,0 +1,7 @@
+% Mass-spring-damper parameters starting with the values from
+% ctms.engin.umich.edu state space model
+m = 1; % Mass = 1.0 kg
+k = 1; % Spring constant = 1.0 N/m
+b = 0.2; % Damping constant = 0.2 Ns/m
+F = 1; % Input Force = 1.0 N
+
