075eddf53775ca7c6ce37f3e26039c8c1a9d071b
Jagadeesh Madinni <jmadinn@clemson.edu>
2024-01-25 16:47:25 -0500

    Exploration files for ordinary kriging

--- spring-damper-mass/.ipynb_checkpoints/spring_damper_system-checkpoint.m
+++ spring-damper-mass/.ipynb_checkpoints/spring_damper_system-checkpoint.m
@@ -1,3 +1,4 @@
+clear vars;
 % Mass-spring-damper parameters starting with the values from
 % ctms.engin.umich.edu state space model
 m = 1; % Mass = 1.0 kg
@@ -11,19 +12,23 @@ B = [0;1/m];
 C = [1 0];
 D = 0;
 t = 0:0.1:60;
-Z = zeros(601,91);
-for k = 1:0.1:10
-    i = int32((k-1)/0.1+1);
+Z = zeros(601,100);
+K = 1:1:100;
+for k = 1:1:100
+    i = k;
     A = [0,1;-k/m,-b/m];
     sys = ss(A,B,C,D);
     Z(:,i)= step(sys,t);
 end 
 
-[X,Y] = meshgrid(1:0.1:10,t);
+[X,Y] = meshgrid(t,K);
 fig = figure();
-surf(X,Y,Z)
-xlabel("Spring Constant-K(N/m)")
-ylabel("Time(s)")
+surf(X,Y,Z')
+ylabel("Spring Constant-K(N/m)")
+xlabel("Time(s)")
 zlabel("Displacement(m)")
 saveas(fig,"/home/jmadinn/ARM_Lab/Spring_Parameter_Plot.png")
-save("/home/jmadinn/ARM_Lab/springParameters.mat","X","Y","Z")
+writematrix(K,'/home/jmadinn/ARM_Lab/springParameters.csv');
+writematrix(t,'/home/jmadinn/ARM_Lab/timesteps.csv');
+writematrix(Z,'/home/jmadinn/ARM_Lab/displacement.csv');
+
