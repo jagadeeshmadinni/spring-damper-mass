a9bfce4a368f811575afed0b03c1ed0bc8f717bf
Jagadeesh Madinni <jmadinn@clemson.edu>
2024-01-26 12:38:54 -0500

    Merge conflicts resolved

--- spring-damper-mass/spring_damper_system.m
+++ spring-damper-mass/spring_damper_system.m
@@ -13,10 +13,9 @@ C = [1 0];
 D = 0;
 t = 0:0.1:60;
 Z = zeros(601,100);
-<<<<<<< HEAD
-=======
+
 K = 1:1:100;
->>>>>>> 075eddf53775ca7c6ce37f3e26039c8c1a9d071b
+
 for k = 1:1:100
     i = k;
     A = [0,1;-k/m,-b/m];
@@ -24,23 +23,19 @@ for k = 1:1:100
     Z(:,i)= step(sys,t);
 end 
 
-<<<<<<< HEAD
-[X,Y] = meshgrid(t,1:1:100);
-=======
 [X,Y] = meshgrid(t,K);
->>>>>>> 075eddf53775ca7c6ce37f3e26039c8c1a9d071b
+
 fig = figure();
 surf(X,Y,Z')
 ylabel("Spring Constant-K(N/m)")
 xlabel("Time(s)")
 zlabel("Displacement(m)")
-<<<<<<< HEAD
+
 %saveas(fig,"Spring Parameter Plot.png")
 %save("C:\Users\jmadinn\Documents\Jagadeesh\springParameters.mat","X","Y","Z")
-=======
+
 saveas(fig,"/home/jmadinn/ARM_Lab/Spring_Parameter_Plot.png")
 writematrix(K,'/home/jmadinn/ARM_Lab/springParameters.csv');
 writematrix(t,'/home/jmadinn/ARM_Lab/timesteps.csv');
 writematrix(Z,'/home/jmadinn/ARM_Lab/displacement.csv');
 
->>>>>>> 075eddf53775ca7c6ce37f3e26039c8c1a9d071b
